714b6efb98b3154d9c4614f18688b4df52a717c1
ShrutiKumariSingh <kumarisarita42416@gmail.com>
2024-11-27 20:23:06 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,20 @@
-#Q2. Find the sum 1+2+3+4...+100 
-def sum_of_num(n):
-    sum = 0
-    for i in range(1, n+1): 
-        sum+=i
-    return sum
-    
-sum_of_num = sum_of_num(100)
-print("Sum of numbers from 1-100 will be:", sum_of_num)
+# Q2. The distance between two cities (in km.) is input through the keyboard. Write a program to 
+# convert and print this distance in meters, feet, inches and centimeters.
+
+distance_between_two_cities = float(input("Enter the distance between two cities(in km): "));
+
+# 1 kilometer = 1000 metres
+km_to_m = distance_between_two_cities * 1000;
+print(f"Distance between two cities in metres: {km_to_m} m");
+
+# 1 kilometer = 3280.84 feet
+km_to_feet = distance_between_two_cities * 3280.84;
+print(f"Distance between two cities in feet: {km_to_feet} feet");
+
+# 1 kilometer = 39,370.1 inches
+km_to_inches = distance_between_two_cities * 39370.1;
+print(f"Distance between the two cities in inches: {km_to_inches} inches")
+
+# 1 kilometer = 100,000 centimeters
+km_to_cm = distance_between_two_cities * 100000;
+print(f"Distance between two cities in centimetre: {km_to_cm} cm");
