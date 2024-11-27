ec105830f8df80fd137ce8a3b18add181b572bbb
ShrutiKumariSingh <kumarisarita42416@gmail.com>
2024-11-27 20:16:51 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+# Q1. Ramesh’s basic salary is input through the keyboard. His dearness allowance is 40% of basic 
+# salary, and house rent allowance is 20% of basic salary. Write a program to calculate his gross 
+# salary.
+
+basic_salary = float(input("Enter Basic salary of Ramesh: "));
+dearness_allowance = 0.4 * basic_salary;
+house_rent = 0.2 * basic_salary;
+gross_salary = basic_salary + dearness_allowance + house_rent;
+print(f"Gross salary of Ramesh: {gross_salary}");
